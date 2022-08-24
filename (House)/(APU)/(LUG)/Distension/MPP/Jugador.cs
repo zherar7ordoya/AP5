ae6c36947a3a2786c6f418886850fb9a78679394
zherar7ordoya <zherar7ordoya@outlook.com>
2022-08-24 08:45:04 -0300

    2022-08-24

--- (House)/(APU)/(LUG)/Distension/MPP/Jugador.cs
+++ (House)/(APU)/(LUG)/Distension/MPP/Jugador.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 using System.Data;
-using System.Text.RegularExpressions;
 
 namespace MPP
 {
--- (House)/(APU)/(LUG)/Distension/UI/JugadorRegistroForm.cs
+++ (House)/(APU)/(LUG)/Distension/UI/JugadorRegistroForm.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
-using System.Xml.Linq;
 
 namespace UI
 {
