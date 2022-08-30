616b94043bfc2ae68ff98d6fa690f25033a365d0
zherar7ordoya <zherar7ordoya@outlook.com>
2022-08-30 12:53:39 -0300

    2022-08-30

--- PRO/Clase 02-1.py
+++ PRO/Clase 02-1.py
@@ -5,14 +5,15 @@ print ("EJERCICIO 1")
 def sumar(x, y):
 	return x + y
 
-sumar(2, 3)
+respuesta = sumar(2, 3)
+print(respuesta)
 
 print ("===========")
 print ("EJERCICIO 2")
 
 def sumar2lecturas():
-	a = eval(input("1er número:"))
-	b = eval(input("2do número:"))
+	a = eval(input("1er número: "))
+	b = eval(input("2do número: "))
 	print("Total: ", a + b)
 
 sumar2lecturas()
@@ -28,7 +29,8 @@ def maximo(a, b):
 	else:
 		return "Son iguales"
 
-maximo(5, 5)
+respuesta = maximo(5, 5)
+print(respuesta)
 
 print ("===========")
 print ("EJERCICIO 4")
@@ -41,10 +43,70 @@ def signo(a):
 	else:
 		return 0
 
-signo(5)
-signo(-5)
-signo(0)
+respuesta = signo(5)
+print(respuesta)
 
 print ("===========")
 print ("EJERCICIO 5")
 
+def sumaImpares(n):
+	acumulador = 0
+	for x in range(n * 2):
+		if (x % 2) != 0: acumulador += x
+	return acumulador
+
+a = eval(input("Cantidad nros: "))
+respuesta = sumaImpares(int(a))
+print(respuesta)
+
+print ("===========")
+print ("EJERCICIO 6")
+
+def mayorExponente(a, n):
+	k = 0
+	while(n % a == 0):
+		n = n / a
+		k += 1
+	return k
+
+respuesta = mayorExponente(2, 40)
+print(respuesta)
+
+print ("===========")
+print ("EJERCICIO 7")
+
+def factorial(n):
+	resultado = 1
+	for x in range(1, n + 1):
+		resultado *= x
+	return resultado
+
+respuesta = factorial(4)
+print(respuesta)
+
+print ("===========")
+print ("EJERCICIO 8")
+
+def suma(xs):
+	r = 0
+	for x in xs:
+		r += x
+	return r
+
+respuesta = suma([3, 2, 5])
+print(respuesta)
+
+print ("===========")
+print ("EJERCICIO 9")
+
+def fact(n):
+	"""Función que calcula un factorial mediante recursividad.
+	n: Cantidad de números
+	retorno: El factorial calculado."""
+	if n == 0:
+		return 1
+	else:
+		return n * fact(n - 1)
+
+respuesta = fact(4)
+print(respuesta)
