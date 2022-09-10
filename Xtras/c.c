ab11424247133de7bcfc7fd34d331475f2706f3c
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-09-10 02:10:48 -0300

    2022-09-10

---
+++ Xtras/c.c
@@ -0,0 +1,13 @@
+// This is a single line comment in C/C++
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
+
+/* This is a multi-line comment in C/C++ */
+
+/* This is slightly long
+multi line comment in C/C++ */
--- Xtras/py.py
+++ Xtras/py.py
@@ -1,4 +1,23 @@
-"""_summary_
+""" @author: Gerardo
+    @date: 2022-09-09
+    @description: Colores en Python
 """
 
-print ("Gerardo Tordoya")
+# Run the script
+
+class Vector:
+    """Crea el vector"""
+    def __init__(self, param1 = "Hello ", param2 = "World!"):
+        self.xth = param1
+        self.yth = param2
+
+    def muestra(self):
+        """Muestra el vector"""
+        print(self.xth, self.yth)       # Se refiere a los atributos de la clase
+        print("Muestra concluída...")   # Se refiere a un string
+
+    def __str__(self):
+        return self.xth + self.yth
+
+VECTOR = Vector()
+VECTOR.muestra()
