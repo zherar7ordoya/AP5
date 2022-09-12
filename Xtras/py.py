e07cfecb164511f4c5d175ddc10a491ec3858705
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-09-12 01:26:19 -0300

    2022-09-12

--- Xtras/py.py
+++ Xtras/py.py
@@ -1,9 +1,14 @@
-""" @author: Gerardo
-    @date: 2022-09-09
-    @description: Colores en Python
+#!/usr/bin/env python
+""" *------------------>= [DESCRIPCIÓN DE ESTE MÓDULO] <=----------------------*
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
 """
+__author__ = "Gerardo Tordoya"
+__date__ = "2022/09/12"
 
-# Run Forest, run!
+# *------------------------->= [Run Forest, run!] <=---------------------------*
 
 class Vector:
     """Crea el vector"""
