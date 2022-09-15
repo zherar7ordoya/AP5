a05ed3ba232c4ad91040e15cf4ffb4ed0126a00c
zherar7ordoya <zherar7ordoya@outlook.com>
2022-09-15 12:49:15 -0300

    2022-09-15

--- PRO/clase05x4.py
+++ PRO/clase05x4.py
@@ -1,121 +1,104 @@
 #!/usr/bin/env python
+
 """ *------------------>= [DESCRIPCIÓN DE ESTE MÓDULO] <=----------------------*
 Ejercicio del banco (usando estructurada).
-Archivos consultados:
-                    Menu.psc
-                    Mayores.psc
-                    Sucursales.psc
-                    Subprocesos.psc
-NOTA: Recordar configurar arreglos a base 0.
+
+Nota:
+En Python no hay forma de elegir como pasar una variable a una funcion (por
+referencia o por valor). Las variables "inmutables" (str, int, float, bool) se
+pasan siempre por copia mientras que las demas (listas, objetos, etc.) se pasan
+siempre por referencia.
 """
 __author__ = "Gerardo Tordoya"
-__date__ = "2022-09-XX"
-
-# *------------------------->= [Run Forest, run!] <=---------------------------*
+__date__ = "2022-09-15"
 
 
-
-# """ *------------------------>= [PROCEDIMIENTOS] <=----------------------------*
-# En python no hay forma de elegir como pasar una variable a una
-# funcion (por referencia o por valor). Las variables "inmutables"
-# (str, int, float, bool) se pasan siempre por copia mientras que
-# las demas (listas, objetos, etc.) se pasan siempre por referencia.
-# Esto coincide con el comportamiento por defecto en pseint, pero
-# cuando utiliza las palabras clave "Por Referencia" para
-# alterarlo la traducción no es correcta.
-
-# INFORMAR
+# *--------------------------->= [PROCEDIMIENTOS] <=---------------------------*
 
 def detallar_cuenta():
     """ Imprime los datos de la cuenta """
+    print("DATOS DE LA CUENTA")
     print("")
-    print("Datos de la Cuenta")
-    print("------------------")
-    print(cliente[0], " su saldo es: ")
+    print(cliente[0], "su saldo es:")
     for i in range(2):
-        print("En la cuenta ",i," hay $ ",cuentas[i])
+        print("En la cuenta",i ,"hay $", cuentas[i])
     print("")
+    print("Seleccione Opción")
 
-# CLIENTES
+def pausar_pantalla():
+    """ Pausa la pantalla """
+    print("Presione una tecla para continuar...")
+    input()
+    os.system('CLS')
+
+# CLIENTES.---------------------------------------------------------------------
+
 def alta_cliente():
     """ Alta de un nuevo cliente """
     print("Usted ya está dado de alta.")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+    pausar_pantalla()
 
 def baja_cliente():
     """ Baja de un cliente """
     print("No es posible aún realizar esa operación.")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+    pausar_pantalla()
 
 def modificacion_cliente(cadena):
-    """ Modificación de datos del cliente
+    """ Mañoso!
         stackoverflow.com/a/55992372/14009797
-        Mañoso!
     """
-    print(cliente[0], "ingrese su nuevo nombre (y apellido): ")
+    print(cliente[0], "ingrese su nuevo nombre:")
     cadena[0] = input()
     print("Operación exitosa.")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+    pausar_pantalla()
 
-# CUENTAS
+
+# CUENTAS.----------------------------------------------------------------------
+
 def alta_cuenta():
     """ Alta de una nueva cuenta """
     print("No es posible aún realizar esa operación.")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+    pausar_pantalla()
 
 def baja_cuenta():
     """ Baja de una cuenta """
     print("No es posible aún realizar esa operación.")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+    pausar_pantalla()
 
 def modificacion_cuenta():
     """ Modificación de datos de una cuenta """
     print("Solo puede modificar los saldos de sus cuentas.")
     print("Para ello, elija una operación del menú principal.")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+    pausar_pantalla()
 
-# OPERACIONES
+
+# OPERACIONES.------------------------------------------------------------------
+
 def depositar():
     """ Deposito de dinero en una cuenta """
     importe = int()
-    print("Ingrese importe a depositar: ")
+    print("Ingrese importe a depositar:")
     importe = int(input())
     cuentas[cuenta] += importe
     print("Operación exitosa.")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+    pausar_pantalla()
 
 def retirar():
     """ Retiro de dinero de una cuenta """
     importe = int()
-    print("Ingrese importe a retirar: ")
+    print("Ingrese importe a retirar:")
     importe = int(input())
     if cuentas[cuenta]>importe:
         cuentas[cuenta] = cuentas[cuenta]-importe
         print("Operación exitosa.")
     else:
-        print("Saldo insuficiente para hacer un retiro")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+        print("Saldo insuficiente para hacer un retiro.")
+    pausar_pantalla()
 
 def transferir():
     """ Transferencia entre cuentas del mismo cliente """
     importe = int()
-    print("Ingrese importe a transferir: ")
+    print("Ingrese importe a transferir:")
     importe = int(input())
     if cuentas[cuenta]>importe:
         if cuenta==0:
@@ -127,98 +110,69 @@ def transferir():
             cuentas[0] = cuentas[0]+importe
             print("Operación exitosa.")
     else:
-        print("Saldo insuficiente para hacer un retiro")
-    print("Presione un tecla para continuar...")
-    input() # a diferencia del pseudocódigo, espera un Enter, no cualquier tecla
-    print("") # no hay forma directa de borrar la pantalla con Python estandar
+        print("Saldo insuficiente para hacer esa transferencia.")
+    pausar_pantalla()
 
-################################################################################
 
+#*-------------------------->= [PROGAMA PRINCIPAL] <=--------------------------*
 
 if __name__ == '__main__':
 
     import os
     os.system('CLS')
 
-    # GLOBALES
-    #cuenta = 0
-    #cuenta = int()
-    cuentas = [int() for ind0 in range(2)]
-
-    # INICIALIZACIÓN
+    print("Ingrese su nombre (y apellido):")
+    cliente = [input()]
     print("")
-    print("Ingrese su nombre (y apellido): ")
-    nya = input()
-    cliente = [nya]
-    #cuenta = 0
+    cuentas = [int() for idx in range(2)]
     cuentas[0] = 0
     cuentas[1] = 0
 
-    detallar_cuenta()
+    while True:
+        detallar_cuenta()
+        print("""
+        1) Alta de Cliente
+        2) Baja de Cliente
+        3) Modificación de Cliente
+        4) Alta de Cuenta
+        5) Baja de Cuenta
+        6) Modificación de Cuenta
+        7) Depositar
+        8) Retirar
+        9) Transferir
+        10) Salir
+        """)
 
-    while True: # no hay 'repetir' en python
-        print("Operatoria Bancaria")
-        print("-------------------")
-        print("Seleccione una opción:")
-        print("1) Alta de Cliente")
-        print("2) Baja de Cliente")
-        print("3) Modificación de Cliente")
-        print("4) Alta de Cuenta")
-        print("5) Baja de Cuenta")
-        print("6) Modificación de Cuenta")
-        print("7) Realizar Depósito")
-        print("8) Realizar Retiro")
-        print("9) Realizar Transferencia")
-        print("10) Salir")
-        operacion = float(input())
-        if operacion==1:
-            # Alta de Cliente
-            alta_cliente()
-            detallar_cuenta()
-        elif operacion==2:
-            # Baja de Cliente
-            baja_cliente()
-            detallar_cuenta()
-        elif operacion==3:
-            # Modificación de Cliente
-            modificacion_cliente(cliente)
-            detallar_cuenta()
-        elif operacion==4:
-            # Alta de Cuenta
-            alta_cuenta()
-            detallar_cuenta()
-        elif operacion==5:
-            # Baja de Cuenta
-            baja_cuenta()
-            detallar_cuenta()
-        elif operacion==6:
-            # Modificación de Cuenta
-            modificacion_cuenta()
-            detallar_cuenta()
-        elif operacion==7:
-            # Realizar Depósito
-            print("Ingrese CUENTA sobre la que operar (0 ó 1): ")
-            cuenta = int(input())
-            depositar()
-            detallar_cuenta()
-        elif operacion==8:
-            # Realizar Retiro
-            print("Ingrese CUENTA sobre la que operar (0 ó 1): ")
-            cuenta = int(input())
-            retirar()
-            detallar_cuenta()
-        elif operacion==9:
-            # Realizar Transferencia
-            print("Ingrese CUENTA sobre la que operar (0 ó 1): ")
-            cuenta = int(input())
-            transferir()
-            detallar_cuenta()
-        elif operacion==10:
-            # Salir
-            print("Gracias y vuelva pronto.")
-            print("Saliendo...")
-            print("Ahora puede cerrar esta ventana.")
-        else:
-            print("Ingrese una opción válida")
-        if operacion==10:
-            break
+        operacion = int(input())
+
+        match operacion:
+            case 1:
+                alta_cliente()
+            case 2:
+                baja_cliente()
+            case 3:
+                modificacion_cliente(cliente)
+            case 4:
+                alta_cuenta()
+            case 5:
+                baja_cuenta()
+            case 6:
+                modificacion_cuenta()
+            case 7:
+                print("Ingrese cuenta sobre la que operar (0 ó 1):")
+                cuenta = int(input())
+                depositar()
+            case 8:
+                print("Ingrese cuenta sobre la que operar (0 ó 1):")
+                cuenta = int(input())
+                retirar()
+            case 9:
+                print("Ingrese cuenta sobre la que operar (0 ó 1):")
+                cuenta = int(input())
+                transferir()
+            case 10:
+                print("Gracias por utilizar nuestros servicios.")
+                break
+            case _:
+                print("Opción inválida.")
+                pausar_pantalla()
