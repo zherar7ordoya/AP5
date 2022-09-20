c0fce8efdd9fcf0791c27574a91755f174b4510e
zherar7ordoya <zherar7ordoya@outlook.com>
2022-09-20 18:32:16 -0300

    2022-09-20

--- Xtras/WinForms2022Net6/Properties/Resources.Designer.cs
+++ Xtras/WinForms2022Net6/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace WinForms2022Net6.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon gt_logo {
+            get {
+                object obj = ResourceManager.GetObject("gt_logo", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
