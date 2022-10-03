a210d93d52e817a267f0b4bb3bd2ff2e0d0af597
zherar7ordoya <zherar7ordoya@outlook.com>
2022-10-03 02:07:46 -0300

    2022-10-03

--- (EndGame)/(Arquitecturas)/Patterns/SingletonWinforms/Factory.cs
+++ (EndGame)/(Arquitecturas)/Patterns/SingletonWinforms/Factory.cs
@@ -5,7 +5,6 @@ namespace SingletonWinforms
     public static class Factory
     {
         public static IDisposable CrearForm1() => new Form1();
-
         public static IDisposable CrearForm2() => new Form2();
     }
 }
