a06f8604f2c918f8a53a870fc7a85fdfd2aa292c
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-11-05 02:34:10 -0300

    2022-11-05

--- PRO/Tutoriales/repository_pattern.py
+++ PRO/Tutoriales/repository_pattern.py
@@ -1,109 +1,137 @@
 # =========================================
 # Author:      Gerardo Tordoya
-# Create date: 2022-11-04
+# Create date: 2022-11-05
 # Description: Patrón de diseño Repositorio
 # =========================================
 
 # NOTA > No crea la tabla, ya debe existir
 
 import sqlite3
+from colorama import Fore, init
+
+init()
 
 
-
-def get_connection():
-    return sqlite3.connect('test.sqlite')
-
-
-class StoreException(Exception):
-    def __init__(self, message, *errors):
-        Exception.__init__(self, message)
-        self.errors = errors
-
-# --- COMIENZA CLASE BASE -------------------------------------------------------
-class Store:
+# --- CLASE BASE --------------------------------------------------------------
+class BaseStore:
     def __init__(self):
         try:
-            self.conn = get_connection()
+            self.conexion = conectarse()
         except Exception as e:
-            raise StoreException(*e.args, **e.kwargs)
-        self._complete = False
+            raise ExceptionStore(*e.args, **e.kwargs)
+        self._completo = False
 
     def __enter__(self):
+        """ Método mágico para el uso de with que se ejecuta al inicio """
         return self
 
     def __exit__(self, type_, value, traceback):
-        # can test for type and handle different situations
-        self.close()
+        """ Método mágico para el uso de with que se ejecuta al final """
+        self.cerrar()
 
-    def complete(self):
-        self._complete = True
+    def completo(self):
+        self._completo = True
 
-    def close(self):
-        if self.conn:
+    def cerrar(self):
+        if self.conexion:
             try:
-                if self._complete:
-                    self.conn.commit()
+                if self._completo:
+                    self.conexion.commit()
                 else:
-                    self.conn.rollback()
+                    self.conexion.rollback()
             except Exception as e:
-                raise StoreException(*e.args)
+                raise ExceptionStore(*e.args)
             finally:
                 try:
-                    self.conn.close()
+                    self.conexion.close()
                 except Exception as e:
-                    raise StoreException(*e.args)
+                    raise ExceptionStore(*e.args)
 
 
-# --- OBJETOS DE DOMINIO ------------------------------------------------------
-
-class User:
-    def __init__(self, name):
-        self.name = name
+# --- DAL ---------------------------------------------------------------------
+def conectarse():
+    return sqlite3.connect('BDD.sqlite')
 
 
-class Animal:
-    def __init__(self, name):
-        self.name = name
+class ExceptionStore(Exception):
+    def __init__(self, mensaje, *errores):
+        Exception.__init__(self, mensaje)
+        self.errores = errores
+        print(f"\n{Fore.RED}\t{errores[0]}{Fore.RESET}")
 
 
-
-
-# store for User obects
-class UserStore(Store):
-
-    def add_user(self, user):
+# --- MAPPER (CRUD) -----------------------------------------------------------
+class UsuarioStore(BaseStore):
+    def create(self, objeto):
         try:
-            c = self.conn.cursor()
-            # this needs an appropriate table
-            c.execute(f'INSERT INTO user(name) VALUES("{user.name}")')
+            indicador = self.conexion.cursor()
+            indicador.execute(f'INSERT INTO Usuarios(Nombre, Apellido, Domicilio, Comentario) '
+                              f'VALUES("{objeto.nombre}", "{objeto.apellido}", "{objeto.domicilio}", "{objeto.comentario}")')
+            print(f"ID > {indicador.lastrowid}")
         except Exception as e:
-            raise StoreException('error storing user')
+            raise ExceptionStore("ERROR AL CREAR AL USUARIO", *e.args)
 
-
-# store for Animal obects
-class AnimalStore(Store):
-
-    def add_animal(self, animal):
+    def read(self, usuario_id):
         try:
-            c = self.conn.cursor()
-            # this needs an appropriate table
-            c.execute(f'INSERT INTO animal(name) VALUES("{animal.name}")')
+            indicador = self.conexion.cursor()
+            indicador.execute(f'SELECT * FROM Usuarios WHERE UsuarioID = {usuario_id}')
+            return indicador.fetchone()
         except Exception as e:
-            raise StoreException('error storing animal')
+            raise ExceptionStore("ERROR AL LEER AL USUARIO", *e.args)
+
+    def update(self, objeto):
+        try:
+            indicador = self.conexion.cursor()
+            indicador.execute(f'UPDATE Usuarios SET Nombre = "{objeto.nombre}", Apellido = "{objeto.apellido}", '
+                              f'Domicilio = "{objeto.domicilio}", Comentario = "{objeto.comentario}" '
+                              f'WHERE UsuarioID = {objeto.usuario_id}')
+        except Exception as e:
+            raise ExceptionStore("ERROR AL ACTUALIZAR AL USUARIO", *e.args)
+
+    def delete(self, usuario_id):
+        try:
+            indicador = self.conexion.cursor()
+            indicador.execute(f'DELETE FROM Usuarios WHERE UsuarioID = {usuario_id}')
+        except Exception as e:
+            raise ExceptionStore("ERROR AL ELIMINAR AL USUARIO", *e.args)
 
 
-# do something
+# --- ENTIDADES ---------------------------------------------------------------
+class Usuario:
+    def __init__(self, nombre, apellido, domicilio, comentario):
+        self.usuario_id = 0
+        self.nombre = nombre
+        self.apellido = apellido
+        self.domicilio = domicilio
+        self.comentario = comentario
+
+
+# --- TEST --------------------------------------------------------------------
 try:
-    with UserStore() as user_store:
-        user_store.add_user(User('John Doe'))
-        user_store.complete()
+    with UsuarioStore() as usuario_store:
+        # Crear
+        usuario = Usuario("Gerardo", "Tordoya", "Alte. Brown", "Prueba de ingreso")
+        usuario_store.create(usuario)
+        usuario_store.completo()
+        print(f"{Fore.GREEN}USUARIO CREADO{Fore.RESET}")
 
-    with AnimalStore() as animal_store:
-        animal_store.add_animal(Animal('Dog'))
-        animal_store.add_animal(Animal('Pig'))
-        animal_store.add_animal(Animal('Cat'))
-        animal_store.add_animal(Animal('Wolf'))
-        animal_store.complete()
-except StoreException as error:
-    # exception handling here
+        # Leer
+        usuario = usuario_store.read(7)
+        usuario_store.completo()
+        print(usuario)
+        print(f"{Fore.GREEN}USUARIO LEÍDO{Fore.RESET}")
+
+        # Actualizar
+        usuario = Usuario("Gerardo", "Tordoya", "Alte. Brown", "Prueba de actualización")
+        usuario.usuario_id = 2
+        usuario_store.update(usuario)
+        usuario_store.completo()
+        print(f"{Fore.GREEN}USUARIO ACTUALIZADO{Fore.RESET}")
+
+        # Eliminar
+        usuario_store.delete(3)
+        usuario_store.completo()
+        print(f"{Fore.GREEN}USUARIO ELIMINADO{Fore.RESET}")
+
+except ExceptionStore as error:
     print(error)
