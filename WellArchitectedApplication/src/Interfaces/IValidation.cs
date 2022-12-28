df578152d76a2641afaaca0af8325c9ecb12f3f3
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-12-28 20:06:00 -0300

    2022-12-28

--- WellArchitectedApplication/src/Interfaces/IValidation.cs
+++
@@ -1,9 +0,0 @@
-﻿// Validation Interface that has a Validation class and is Generic Interface where we can inject Type
-
-namespace Interfaces
-{
-    public interface IValidation<InjectType>
-    {
-        void Validate(InjectType obj);
-    }
-}
