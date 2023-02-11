064fc0daad63137219a4dbb0faee60880400d18d
Gerardo Rodolfo Tordoya <zherar7ordoya@outlook.com>
2023-02-11 00:01:09 -0300

    2023-02-11

--- (EndGame)/[POO] Cardacci/Itinerario 3/Grilla/Ejemplo1/Properties/AssemblyInfo.cs
+++ (EndGame)/[POO] Cardacci/Itinerario 3/Grilla/Ejemplo1/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Ej0030")]
+[assembly: AssemblyTitle("Ejemplo1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ej0030")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Ejemplo1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("682284a1-1356-4741-9341-1adf0420038f")]
+[assembly: Guid("74e90326-4cf5-4de7-bef9-e46ec7949538")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
@@ -28,8 +29,8 @@ using System.Runtime.InteropServices;
 //      Número de compilación
 //      Revisión
 //
-// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
-// mediante el carácter '*', como se muestra a continuación:
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
