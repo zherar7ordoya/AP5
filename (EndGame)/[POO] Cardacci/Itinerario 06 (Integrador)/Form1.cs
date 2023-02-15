1b54cd2b4857885f8eee7d736759f2ddc2721259
Gerardo Rodolfo Tordoya <zherar7ordoya@outlook.com>
2023-02-15 13:28:32 -0300

    2023-02-15

---
+++ (EndGame)/[POO] Cardacci/Itinerario 06 (Integrador)/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Integrador
+{
+    public partial class Form1 : Form
+    {
+        Concesionario _c;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _c= new Concesionario();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RefrescarGrilla(dataGridView1, _c.RetornaListaPersonas());
+        }
+
+
+        void RefrescarGrilla(DataGridView pDGV, object pObject)
+        {
+            pDGV.DataSource = null;
+            pDGV.DataSource = pObject;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _c.AgregarPersona(new Persona(
+                    Interaction.InputBox("DNI"),
+                    Interaction.InputBox("Nombre"),
+                    Interaction.InputBox("Apellido")));
+                RefrescarGrilla(dataGridView1, _c.RetornaListaPersonas());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.Rows.Count > 0)
+                {
+                    _c.BorrarPersona(dataGridView1.SelectedRows[0].DataBoundItem as Persona);
+                    RefrescarGrilla(dataGridView1, _c.RetornaListaPersonas());
+                }
+                else
+                {
+                    throw new Exception("No hay nada para borrar");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.Rows.Count > 0)
+                {
+                    Persona _auxPersona = dataGridView1.SelectedRows[0].DataBoundItem as Persona;
+                    if (_auxPersona != null)
+                    {
+                        _auxPersona.Nombre = Interaction.InputBox("Nombre", "Modificación", _auxPersona.Nombre);
+                        _auxPersona.Apellido = Interaction.InputBox("Apellido", "Modificación", _auxPersona.Apellido);
+                        _c.ModificarPersona(_auxPersona);
+                        RefrescarGrilla(dataGridView1, _c.RetornaListaPersonas());
+                    }
+                    else
+                    {
+                        throw new Exception("No existe la persona a modificar");
+                    }
+                }
+                else
+                {
+                    throw new Exception("No hay nada para modificar");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ (EndGame)/[POO] Cardacci/Itinerario 06 (Integrador)/Persona.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrador
+{
+    class Persona
+    {
+        List<Auto> _listaAuto;
+        public Persona() { _listaAuto = new List<Auto>(); }
+        public Persona(
+            string pDNI,
+            string pNombre,
+            string pApellido) : this()
+        {
+            DNI = pDNI;
+            Nombre = pNombre;
+            Apellido = pApellido;
+        } 
+
+        // TODO Agrgar propiedades de persona
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+
+        public void AgregarAuto(Auto pAuto) { _listaAuto.Add(new Auto(pAuto.Patente, pAuto.Marca, pAuto.Modelo, pAuto.Axo, pAuto.Precio, pAuto.Dueno())); }
+
+
+
+        // TODO borrar Auto de la lista
+        // TODO Retornar lista de  Autos 
+
+        public void BorrarAuto(Auto pAuto)
+        {
+            /* ¿Por qué es necesario esto? Porque el objeto que recibo como
+             * parámetro no es el mismo que tengo en la lista. El Try/Catch es
+             * por si mando un NULL (no se puede remover un NULL) */
+            try
+            {
+                Auto _auxAuto = _listaAuto.Find(x => x.Patente == pAuto.Patente);
+                if (_auxAuto != null)
+                {
+                    _listaAuto.Remove(_auxAuto);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Auto> RetornaListaAutos()
+        {
+            List<Auto> _aux_listaAuto = new List<Auto>();
+            foreach (Auto _a in _listaAuto)
+            {
+                _aux_listaAuto.Add(new Auto(
+                    _a.Patente,
+                    _a.Marca,
+                    _a.Modelo,
+                    _a.Axo,
+                    _a.Precio,
+                    _a.Dueno()));
+            }
+            return _aux_listaAuto;
+        }
+
+
+        ~Persona()
+        {
+            _listaAuto = null;
+        }
+    }
+}
+
