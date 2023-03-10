1489639e63a80356334cdfda890e167d54eaa5b0
Gerardo Rodolfo Tordoya <zherar7ordoya@outlook.com>
2023-03-10 02:11:45 -0300

    2023-03-10

---
+++ (EndGame)/[PRO] Cardacci/(FINAL[!])/Práctica para el Final/Listas2/main.py
@@ -0,0 +1,26 @@
+# -*- coding: cp1252 -*-
+
+"""
+Escribir un programa que almacene las asignaturas de un curso (por ejemplo,
+Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al
+usuario la nota que ha sacado en cada asignatura y elimine de la lista las
+asignaturas aprobadas. Al final el programa debe mostrar por pantalla las
+asignaturas que el usuario tiene que repetir.
+"""
+
+materias = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+notas = []
+
+for materia in materias:
+    nota = input(f"¿Qué nota has sacado en {materia}? ")
+    notas.append(nota)
+
+for i in range(len(materias) - 1, -1, -1):
+    if int(notas[i]) >= 7:
+        materias.pop(i)
+        notas.pop(i)
+
+print("Tienes que repetir las siguientes asignaturas:")
+
+for i in range(len(materias)):
+    print(f"{materias[i]}")
